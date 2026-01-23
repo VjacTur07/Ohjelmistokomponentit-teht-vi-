5438ab38ca9118831d3eea0542639da6ba80296b
VjacTur07 <s2401265@edu.bc.fi>
2026-01-23 12:37:23 +0200

    Tervehdys

---
+++ Komponentit/src/App.jsx
@@ -0,0 +1,12 @@
+import * as React from "react";
+import Hello from "./Tervehdys";
+
+function App() {
+  return (
+    <div>
+      <Hello />
+    </div>
+  );
+}
+
+export default App;
---
+++ Komponentit/src/Tervehdys.jsx
@@ -0,0 +1,8 @@
+import * as React from "react";
+
+function Hello() {
+  const name = "Vjaceslavs";
+  return <p>Hei {name}!</p>;
+}
+
+export default Hello;
