edfffb4ef83bbbea2505456a1431a7f171e94527
VjacTur07 <s2401265@edu.bc.fi>
2026-01-23 12:53:14 +0200

    OpiskelijaTiedot

--- Komponentit/src/App.jsx
+++ Komponentit/src/App.jsx
@@ -1,10 +1,12 @@
 import * as React from "react";
 import Hello from "./Tervehdys";
+import OpiskelijaTiedot from "./OpiskelijaTiedot";
 
 function App() {
   return (
     <div>
       <Hello />
+      <OpiskelijaTiedot />
     </div>
   );
 }
---
+++ Komponentit/src/OpiskelijaTiedot.jsx
@@ -0,0 +1,18 @@
+import * as React from "react";
+
+function OpiskelijaTiedot() {
+  const opiskelija = {
+    nimi: "Matti Meikäläinen",
+    ika: 16,
+    kurssi: "Reactin perusteet",
+  };
+  return (
+    <div>
+      <p>Nimi: {opiskelija.nimi}</p>
+      <p>Ikä: {opiskelija.ika}</p>
+      <p>Kurssi: {opiskelija.kurssi}</p>
+    </div>
+  );
+}
+
+export default OpiskelijaTiedot;
