5a669a6e6dd14600eb084ab62b89b79dd8d6d8e7
VjacTur07 <s2401265@edu.bc.fi>
2026-03-12 13:28:40 +0200

    Korjaa laskuri komponenti

---
+++ Korjaa komponentit/Korjaa Komponentit/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Laskuri from "./laskuri";
+
+function App() {
+  return (
+    <div>
+      <h1>Laskuri Komponenti</h1>
+      <Laskuri />
+    </div>
+  );
+}
+
+export default App;
